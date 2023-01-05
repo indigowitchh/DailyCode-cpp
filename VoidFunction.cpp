890e00b145d4d5210c64345548dcfe9bc09f3ad7
do i give a darn <114239691+indigowitchh@users.noreply.github.com>
2023-01-05 08:28:45 -0700

    Create VoidFunction.cpp

---
+++ VoidFunction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//function declaration
+void printHello();
+
+int main() {
+	//function call
+	printHello();
+}
+
+//function definition
+void printHello() {
+	cout << "Hello World" << endl;
+}
