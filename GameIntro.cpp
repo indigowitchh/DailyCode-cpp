40b9f1300c0e167a37012fd10a64f772c4d32d50
do i give a darn <114239691+indigowitchh@users.noreply.github.com>
2023-01-05 08:15:35 -0700

    Create GameIntro.cpp

---
+++ GameIntro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello? Hello, hello! Uh, I wanted to record a message for you, to help you get settled in on your first night." << endl;
+	cout << "Um, I actually worked in that office before you." << endl;
+	cout << "I'm...finishing up my last week now as a matter of fact so...I know it can be a little overwhelming, but I'm here to tell you:" << endl;
+	cout << "there's nothing to worry about, uhh, you'll do fine!" << endl;
+	cout << "So...let's just focus on getting you through your first week. Ok?" << endl;
+}
